8a5f1debdfff2c6febeffe93c8ea9ec868c6736a
Victor1890 <victorrosariodeveloper@gmail.com>
2024-07-15 10:38:55 -0400

    feat: :triangular_flag_on_post: add @tanstack/react-table dependency and PaymentManagement component

--- src/components/features/ui-elements/examples/payment-management/example.tsx
+++ src/components/features/ui-elements/examples/payment-management/example.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import {
     Table,
     TableHeader,
@@ -34,7 +34,7 @@ import {
     DropdownItem
 } from "@nextui-org/dropdown";
 import { Input } from '@nextui-org/input';
-import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/react';
+import { Card, CardBody, CardFooter, CardHeader, cn } from '@nextui-org/react';
 
 type Payment = {
     id: string;
@@ -51,97 +51,86 @@ const data: Payment[] = [
     { id: "bhqecj4p", amount: 721, status: "failed", email: "carmella@hotmail.com" },
 ];
 
-const columns: ColumnDef<Payment>[] = [
-    {
-        id: "select",
-        header: ({ table }) => {
-            const {
-                toggleAllPageRowsSelected,
-                getIsAllPageRowsSelected,
-                getIsSomePageRowsSelected,
-            } = table;
-
-            const isActive = getIsAllPageRowsSelected() || (getIsSomePageRowsSelected() && "indeterminate") as boolean;
-
-            return (
-                <Checkbox
-                    isSelected={getIsAllPageRowsSelected() || (getIsSomePageRowsSelected() && "indeterminate") as boolean}
-                    onValueChange={(value) => {
-                        toggleAllPageRowsSelected(!!value);
-                        console.log("table: ", {
-                            value,
-                            isActive,
-                            isActive1: getIsAllPageRowsSelected(),
-                            isActive2: getIsSomePageRowsSelected() && "indeterminate"
-                        });
-                    }}
-                    aria-label="Select all"
-                />
-            )
-        },
-        cell: ({ row }) => (
-            <Checkbox
-                isSelected={row.getIsSelected()}
-                onValueChange={(value) => row.toggleSelected(!!value)}
-                aria-label="Select row"
-            />
-        ),
-        enableSorting: false,
-        enableHiding: false,
-    },
-    {
-        accessorKey: "status",
-        header: "Status",
-        cell: ({ row }) => <div className="capitalize">{row.getValue("status")}</div>,
-    },
-    {
-        accessorKey: "email",
-        header: ({ column }) => (
-            <Button variant="flat" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-                Email
-                <ArrowUpDown className="ml-2 h-4 w-4" />
-            </Button>
-        ),
-        cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
-    },
-    {
-        accessorKey: "amount",
-        header: () => <div className="text-right">Amount</div>,
-        cell: ({ row }) => {
-            const amount = parseFloat(row.getValue("amount"));
-            const formatted = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount);
-            return <div className="text-right font-medium">{formatted}</div>;
-        },
-    },
-    {
-        id: "actions",
-        enableHiding: false,
-        cell: ({ row }) => {
-            const payment = row.original;
-            return (
-                <Dropdown>
-                    <DropdownTrigger>
-                        <Button variant="flat" className="h-8 w-8 p-0">
-                            <span className="sr-only">Open menu</span>
-                            <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                    </DropdownTrigger>
-                    <DropdownMenu aria-label="Actions">
-                        <DropdownSection>
-                            <DropdownItem key="copy" onClick={() => navigator.clipboard.writeText(payment.id)}>
-                                Copy payment ID
-                            </DropdownItem>
-                            <DropdownItem key="viewCustomer">View customer</DropdownItem>
-                            <DropdownItem key="viewPayment">View payment details</DropdownItem>
-                        </DropdownSection>
-                    </DropdownMenu>
-                </Dropdown>
-            );
-        },
-    },
-];
-
 export default function DataTableDemo() {
+
+    const columns: ColumnDef<Payment>[] = [
+        {
+            id: "select",
+            header: ({ table }) => {
+                const isSelected = table.getIsAllPageRowsSelected();
+
+                return (
+                    <Checkbox
+                        isSelected={isSelected}
+                        onValueChange={(value) => {
+                            console.log("isSelected: ", isSelected)
+                            table.toggleAllPageRowsSelected(!!value)
+                        }}
+                        aria-label="Select all"
+                    />
+                )
+            },
+            cell: ({ row }) => (
+                <Checkbox
+                    isSelected={row.getIsSelected()}
+                    onValueChange={(value) => row.toggleSelected(!!value)}
+                    aria-label="Select row"
+                />
+            ),
+            enableSorting: false,
+            enableHiding: false,
+        },
+        {
+            accessorKey: "status",
+            header: "Status",
+            cell: ({ row }) => <p className="capitalize">{row.getValue("status")}</p>,
+        },
+        {
+            accessorKey: "email",
+            header: ({ column }) => (
+                <Button className='text-foreground-500 font-semibold' variant="light" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
+                    Email
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            ),
+            cell: ({ row }) => <p className="lowercase">{row.getValue("email")}</p>,
+        },
+        {
+            accessorKey: "amount",
+            header: () => <div className="text-right">Amount</div>,
+            cell: ({ row }) => {
+                const amount = parseFloat(row.getValue("amount"));
+                const formatted = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(amount);
+                return <p className="text-right font-medium">{formatted}</p>;
+            },
+        },
+        {
+            id: "actions",
+            enableHiding: false,
+            cell: ({ row }) => {
+                const payment = row.original;
+                return (
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button variant="flat" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Actions">
+                            <DropdownSection>
+                                <DropdownItem key="copy" onClick={() => navigator.clipboard.writeText(payment.id)}>
+                                    Copy payment ID
+                                </DropdownItem>
+                                <DropdownItem key="viewCustomer">View customer</DropdownItem>
+                                <DropdownItem key="viewPayment">View payment details</DropdownItem>
+                            </DropdownSection>
+                        </DropdownMenu>
+                    </Dropdown>
+                );
+            },
+        },
+    ]
 
     const [rowSelection, setRowSelection] = useState({})
     const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({})
@@ -170,14 +159,16 @@ export default function DataTableDemo() {
         getFacetedUniqueValues: getFacetedUniqueValues(),
     });
 
+    const header = useMemo(() => table.getHeaderGroups().flatMap(headerGroup => headerGroup.headers), [table])
+
     return (
         <Card className="w-full">
-            <CardHeader className="flex items-center justify-between p-6">
+            <CardHeader className="flex items-center justify-between p-9 pb-2 pt-6">
                 <Input
                     placeholder="Filter emails..."
                     value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
                     onChange={(event) => {
-                        table.getColumn("email")?.setFilterValue(event.target.value);
+                        table?.getColumn("email")?.setFilterValue(event.target.value);
                     }}
                     className="max-w-sm"
                 />
@@ -201,12 +192,16 @@ export default function DataTableDemo() {
                     </DropdownMenu>
                 </Dropdown>
             </CardHeader>
-            <CardBody className="rounded-md">
+            <CardBody className="border-none shadow-none p-6 pb-0 pt-0">
                 <Table
+                    className='border-none shadow-none'
+                    classNames={{
+                        wrapper: "shadow-none"
+                    }}
                     aria-label='Payments'
                     aria-labelledby='Payments'
                 >
-                    <TableHeader columns={table.getHeaderGroups().flatMap(headerGroup => headerGroup.headers)}>
+                    <TableHeader columns={header}>
                         {(header) => (
                             <TableColumn key={header.id}>
                                 {header.isPlaceholder
@@ -215,22 +210,17 @@ export default function DataTableDemo() {
                             </TableColumn>
                         )}
                     </TableHeader>
-                    <TableBody>
-                        {table.getRowModel().rows?.length ? (
-                            table.getRowModel().rows.map((row) => (
-                                <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                                    {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
-                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                        </TableCell>
-                                    ))}
-                                </TableRow>
-                            ))
-                        ) : (
-                            <TableRow>
-                                <TableCell colSpan={columns.length} className="h-24 text-center">
-                                    No results.
-                                </TableCell>
+                    <TableBody
+                        emptyContent={"No results"}
+                        items={table.getRowModel().rows}
+                    >
+                        {(item) => (
+                            <TableRow key={item.id} data-state={item.getIsSelected() && "selected"}>
+                                {item.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
                             </TableRow>
                         )}
                     </TableBody>
