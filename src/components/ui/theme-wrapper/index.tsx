03a3f61d23e925f628130eea5a16dee8fb8e4038
Victor Jesús Rosario Vásquez <46900196+Victor1890@users.noreply.github.com>
2024-07-20 21:13:48 -0400

    refactor: :recycle: prepare the base, wrapper provider

--- src/components/ui/theme-wrapper/index.tsx
+++ src/components/ui/theme-wrapper/index.tsx
@@ -1,13 +1,8 @@
 import { ThemeProvider } from "next-themes";
-import { DetailedHTMLProps, HTMLAttributes } from "react";
+import { ThemeProviderProps } from "next-themes/dist/types";
 
-interface ThemeWrapperProps extends HTMLAttributes<HTMLDivElement> {
-}
+interface ThemeWrapperProps extends ThemeProviderProps {}
 
-export function ThemeWrapper({ children, ...props }: ThemeWrapperProps) {
-    return (
-        <div {...props}>
-            {children}
-        </div>
-    )
+export function ThemeWrapper(props: ThemeWrapperProps) {
+  return <ThemeProvider attribute="class" forcedTheme="dark" {...props} />;
 }
