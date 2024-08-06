e06f2d99d9c19e267200e5e2f17440f0ef323c5a
Victor1890 <victorrosariodeveloper@gmail.com>
2024-08-06 11:27:53 -0400

    refactor: :label: update THEME_TEMPLATE_OUTPUT function to use Partial<ThemeColors>

--- src/utils/string.util.ts
+++ src/utils/string.util.ts
@@ -1,19 +1,13 @@
 import { ThemeColors } from "@nextui-org/react";
 
 interface ITHEME_TEMPLATE_OUTPUT {
-  name: string
-  schemas: Record<"light" | 'dark', ThemeColors>
+  schemas: Record<"light" | 'dark', Partial<ThemeColors>>
 }
 
-export const THEME_TEMPLATE_OUTPUT = ({ name, schemas }: ITHEME_TEMPLATE_OUTPUT) => {
-
+export const THEME_TEMPLATE_OUTPUT = ({ schemas }: ITHEME_TEMPLATE_OUTPUT) => {
   const theme = Object.entries(schemas).reduce((acc: any, [schema, colors]) => {
-
-    const label = `${name}-${schema}-theme`;
-  
-    if(!acc[label]) acc[label] = {};
-
-    acc[label] = {
+    if(!acc[schema]) acc[schema] = {};
+    acc[schema] = {
       extend: schema,
       colors
     }
@@ -30,11 +24,20 @@ const config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [
     nextui({
-      themes: ${JSON.stringify(theme, null, 2)}
+      themes: ${JSON.stringify(theme, null, 2).split("\n")
+        .map((line, index, arr) => {
+          if(index === 0) return line;
+          if(index === arr.length - 1) return "".padStart(7, " ") + line;
+          return "".padStart(8, " ") + line;
+        })
+        .join("\n")}
     }),
   ],
 };
