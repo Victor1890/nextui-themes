89051d5cc28ef7812931e1a3092b42304ef32d1c
Victor1890 <victorrosariodeveloper@gmail.com>
2024-08-06 11:52:21 -0400

    feat: :mag: update metadata in layout.tsx and page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,54 @@ import "@/styles/globals.css";
 import { cn } from "@nextui-org/react";
 import { Provider } from "./provider";
 import { fontSans } from "@/config/fonts";
+import { type Metadata } from "next";
+import { siteConfig } from "@/config/site";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  metadataBase: new URL(siteConfig.url),
+  description: siteConfig.description,
+  keywords: siteConfig.keywords,
+  authors: [
+    {
+      name: "Victor J. Rosario V.",
+      url: "https://shadcn.com",
+    },
+  ],
+  creator: "Victor J. Rosario V.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@shadcn",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 export default function RootLayout({
   children,
